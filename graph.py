1803d838f6000eba05ca9afcdc6839e15fa93229
cucuridas@gamil.com <3310223@naver.com>
2023-08-07 14:48:57 +0900

    Divide previous datafarme to use visualization

--- graph.py
+++ graph.py
@@ -1,26 +1,76 @@
 import pandas as pd
-import os
+import csv
 
-FILE_FATH = input()
-TEST_VLUE = os.open("/Users/cucuridas/Desktop/코딩테스트_성능지표/programmers_personality_type_test.csv")
+#FILE_FATH = input()
+TEST_VLUE = "/Users/cucuridas/Desktop/performence_codingtest/programmers_personality_type_test.csv"
+PREPROCESSING_OBJECTS = ["통과","\(","\)","ms","MB"]
 
 def read_csv():
-    df = pd.read_csv(TEST_VLUE)
+    df = pd.read_csv(TEST_VLUE,index_col=0)
     return df 
 
 def preprocessing(df):
     df.fillna(0)
-    df = df.replace(to_replace ='통과',value='',regex=True)
-    return  
+
+    for pre in PREPROCESSING_OBJECTS:
+        df = df.replace(to_replace =pre,value='',regex=True)
+    return  df
 
 # https://pandas.pydata.org/docs/reference/api/pandas.DataFrame.divide.html
 # https://blockchainstudy.tistory.com/45
-def devide_field(df):
-    for column in df.columns.to_list():
-        df[column]['performence'], df[column]['memory'] = df[column].apply(lambda x: x.split(', '))
-    pass
+def create_new_dataframe(df,value):
+    people = df.columns.tolist()
+    multi_columns = []
+    columns = []
+    index = df.index.tolist()
+
+    for i in people:
+        columns.extend([i,i])
+
+    for i in range(0,len(people)):
+        multi_columns.extend(["performence","memory"])
+    
+    new_dataframe = pd.DataFrame(data=value,index = index, columns= [columns,multi_columns])
+    
+    return new_dataframe
+    
+def divide_performence_and_memory(list_values):
+    performence,memory = list_values.split(", ")
+    
+    return performence,memory
+
+
+
 
 if __name__ == "__main__":
     dataframe = read_csv()
     pre_df = preprocessing(dataframe)
-    print(pre_df)
+    index_list = pre_df.index.to_list()
+    value_list = []
+    df = None
+
+    for i in pre_df.columns.to_list():
+        #Divide dataframe value by using string format
+        result = list(map(divide_performence_and_memory,pre_df[i]))
+
+        # i.performence value, i.memory value 
+        perf = list(map(lambda x: float(x[0].strip()),result))
+        mem = list(map(lambda x: float(x[1].strip()),result))
+
+        value_dict = {
+            (i,'performence'):perf,
+            (i,'memory'):mem
+        }
+
+        if not isinstance(df,pd.DataFrame) :
+            df = pd.DataFrame(data=value_dict,columns=([[i,i],['performence','memory']]))
+        else:
+            df = pd.concat([df,pd.DataFrame(data=value_dict,columns=([[i,i],['performence','memory']]))],axis=1)
+        
+    
+        # new_dataframe[i]["memory"] = 
+
+
+    
+
+    
